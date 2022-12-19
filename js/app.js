b5a2b7de0d8e1f64cee5cd57af5981c0e2b439e3
semyonkungurov <s.kungurov8679@mail.ru>
2022-12-19 21:24:36 +0700

    ...

--- js/app.js
+++ js/app.js
@@ -4,6 +4,18 @@ carBtn.addEventListener('click', btnOperation)
 function btnOperation() {
     var random = Math.random();
     if (random > 0.5) {
-        alert('Car have started');
+        carBtn.classList.add('hide');
+        console.log('Car have started');
+        window.setTimeout(engine, 5000);
+        console.log("we wait crash");
+    } else{
+        console.log('Something wrong');
     }
 }
+function engine(){
+    console.log('engine crashed');
+    carBtn.classList.remove('hide');
+}
+
+carBtn = document.querySelector('#start-car');
+carBtn.addEventListener('click', btnOperation)

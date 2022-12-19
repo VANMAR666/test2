84083ab61017d70d0eeb94fa6f0ab80bc68fbaad
semyonkungurov <s.kungurov8679@mail.ru>
2022-12-19 15:58:23 +0700

    ...

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-carBtn=document.querySelector('#start-car');
-carBtn.addEventListener('click',btnOperation)
-function btnOperation(){
+carBtn = document.querySelector('#start-car');
+carBtn.addEventListener('click', btnOperation)
 
+function btnOperation() {
+    var random = Math.random();
+    if (random > 0.5) {
+        alert('Car have started');
+    }
 }
-alert('Hi')

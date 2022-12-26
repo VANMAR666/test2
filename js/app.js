423b9729218d693fb6f6f8e3f31efca93a77f1b8
semyonkungurov <s.kungurov8679@mail.ru>
2022-12-26 22:49:13 +0700

    code edited

--- js/app.js
+++ js/app.js
@@ -1,22 +1,39 @@
-carBtn = document.querySelector('#start-car');
-carBtn.addEventListener('click', btnOperation)
-
 function btnOperation() {
     var random = Math.random();
     if (random > 0.5) {
-        carBtn.classList.add('hide');
-        console.log('Car have started');
-        window.setTimeout(engine, 5000);
-        console.log("we wait crash");
+        carStarted();
     } else {
-        console.log('Something wrong');
+        carCannotBeStarted();
     }
 }
 
-function engine() {
-    console.log('engine crashed');
-    carBtn.classList.remove('hide');
+function engineCrashed() {
+    devLog('engine crashed');
+    drawStatus('Engine have crashed. Car stopped');
+    startButton.classList.remove('hide');
 }
 
-carBtn = document.querySelector('#start-car');
-carBtn.addEventListener('click', btnOperation)
+function drawStatus(status) {
+    statusLabel.innerHTML = status;
+}
+
+function devLog(message) {
+    console.log(message);
+}
+
+function carStarted() {
+    drawStatus('Car have started');
+    devLog('Car have started');
+    startButton.classList.add('hide');
+    window.setTimeout(engineCrashed, 2000);
+    devLog('we wait crash')
+}
+
+function carCannotBeStarted() {
+    devLog('Something wrong');
+    drawStatus('Car can\'t be started. try again!');
+}
+
+var startButton = document.querySelector('#start-car');
+var statusLabel = document.querySelector('#status');
+startButton.addEventListener('click', btnOperation)
